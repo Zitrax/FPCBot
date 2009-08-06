7e74e71168884ff9673deddd28231075d81ce325
Daniel Bengtsson <daniel@bengtssons.info>
2009-08-07 00:13:33 +0200

    Now finds the number of days since the nomination was created.

--- fpc.py
+++ fpc.py
@@ -4,12 +4,19 @@
 
 # TODO: catch exceptions
 
-import wikipedia
+import wikipedia, re, datetime
 
 candPrefix = "Commons:Featured picture candidates/"
 
 class Candidate():
-    """This is one feature picture candidate"""
+    """
+    This is one feature picture candidate.
+
+    TODO:
+    * How to detect edits (multi image nomination) ?
+      imagelinks() is no good it, there might be links that are not nominations
+
+    """
 
     def __init__(self, page):
         """page is a wikipedia.Page object"""
@@ -38,15 +45,41 @@ class Candidate():
             else:
                 self._unknown += 1
 
-        wikipedia.output("%s: S:%02d O:%02d N:%02d U:%02d (%s)" % 
+        wikipedia.output("%s: S:%02d O:%02d N:%02d U:%02d D:%02d (%s)" % 
                          ( self.page.title().replace(candPrefix,'')[0:40].ljust(40),
                            self._support,self._oppose,self._neutral,self._unknown,
+                           self.daysOld(),
                            "Featured" if self.isFeatured() else "Not featured"), 
                          toStdout = True)
 
-    def isFeatured(self):
-        return (self._support >= 2*self._oppose) and self._support >= 5
+    def creationTime(self):
+        """Find the time that this candidate were created"""
+        history = self.page.getVersionHistory(reverseOrder=True,revCount=1)
+        
+        # Could be compiled
+        m = re.search('(\d\d):(\d\d), (\d{1,2}) ([a-z]+) (\d{4})',history[0][1].lower())
+        return  datetime.datetime(int(m.group(5)),
+                                  Month[m.group(4)],
+                                  int(m.group(3)),
+                                  int(m.group(1)),
+                                  int(m.group(2)))
     
+    def daysOld(self):
+        """Find the number of days this nomination has existed"""
+        old = datetime.datetime.now() - self.creationTime()
+        return old.days
+
+    def isFeatured(self):
+        """
+        Find if an image can be featured or not.
+
+        An image can be featured if all of this is true:
+        * There are 5 or more support votes
+        * The ratio of support/oppose >= 2/1
+        * 9 days has passed since nomination
+        """
+        
+        return (self._support >= 2*self._oppose) and self._support >= 5 and self.daysOld() >= 9
     
 
 def findCandidates(page):
@@ -63,6 +96,8 @@ def findCandidates(page):
             #wikipedia.output("Skipping '%s'" % title, toStdout = True)
     return candidates
 
+
+Month  = { 'january':1, 'february':2, 'march':3, 'april':4, 'may':5, 'june':6, 'july':7, 'august':8, 'september':9, 'october':10, 'november':11, 'december':12 }
 
 def main():
 
