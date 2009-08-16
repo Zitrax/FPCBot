20d634240961aa6446e67b5b7e9f22543045b5f0
Daniel Bengtsson <daniel@bengtssons.info>
2009-08-17 01:53:28 +0200

    Removed debug statement

--- fpc.py
+++ fpc.py
@@ -374,7 +374,6 @@ class Candidate():
         # Thanks KODOS for a nice regexp gui
         # This adds ourself first in the list of length 4 and removes the last
         # all in the chosen category
-        print "%s %s" % (category,self.fileName())
         ListPageR = re.compile(r"(^==\s*{{{\s*\d+\s*\|%s\s*}}}\s*==\s*<gallery.*>\s*)(.*\s*)(.*\s*.*\s*)(.*\s*)(</gallery>)" % category, re.MULTILINE)
         new_text = re.sub(ListPageR,r"\1%s\n\2\3\5" % self.fileName(), old_text)
         self.commit(old_text,new_text,page,"Added %s" % self.fileName() )
